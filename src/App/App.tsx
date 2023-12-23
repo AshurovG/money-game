e5e6553269c0943530a8b85c0366441e5012f70d
Georgy <ashurovgeorgy@MacBook-Pro-som-tillhor-Georgy.local>
2023-12-23 18:04:23 +0300

    basic configuration

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import styles from './App.module.scss'
+
+function App() {
+  return (
+    <>
+      <div>
+        Money Game
+      </div>
+    </>
+  )
+}
+
+export default App
